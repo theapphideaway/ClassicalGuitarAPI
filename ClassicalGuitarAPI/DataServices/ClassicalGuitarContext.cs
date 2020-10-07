d4080d3c29ae069cb8b428f6694b1209afa230e4
Ian Schoenrock <63610950+ischoenrock@users.noreply.github.com>
2020-10-06 18:05:31 -0600

    Added new ip and updated photos

--- ClassicalGuitarAPI/DataServices/ClassicalGuitarContext.cs
+++ ClassicalGuitarAPI/DataServices/ClassicalGuitarContext.cs
@@ -9,14 +9,17 @@ namespace ClassicalGuitarAPI.DataServices
 {
     public class ClassicalGuitarContext: DbContext
     {
-        private const string ConnectionString = @"Server=tcp:192.168.0.21,1434;Initial Catalog=123;Persist Security Info=False;User ID=123;Password=123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+        private const string ConnectionString = @"Server=tcp:192.168.0.13,1434;Initial Catalog=classical_guitars;Persist Security Info=False;User ID=ServerAuth;Password=Enter967;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
 
         public DbSet<Guitar> Guitar { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseSqlServer(ConnectionString, builder =>
+            {
+                builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+            });
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(ConnectionString);
         }
     }
 }
